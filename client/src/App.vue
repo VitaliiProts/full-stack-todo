a513e2f01dba8198530bd6091e04a55962bf7400
v.prots <vitalikprocj@gmail.com>
2018-12-25 03:06:30 +0200

    add cliend part

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <post-component msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import PostComponent from "./components/PostComponent.vue";
+
+export default {
+  name: "app",
+  components: {
+    PostComponent
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ client/src/components/PostComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <section>
+    <label for="create-post">Say something...</label>
+    <input type="text" placeholder="Create post" v-model="text">
+    <button @click="createPost">Create post!</button>
+    <hr>
+    <p v-if="error">{{ error }}</p>
+    <div v-for="(post, index) in posts"
+        :item="post"
+        :index="index"
+        :key="post._id"
+        @dblclick="deletePost(post._id)"
+    >
+    <span>{{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}` }}</span>
+    <p>{{ post.text }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import PostService from '../PostService.js';
+
+export default {
+  name: "PostComponent",
+  data() {
+    return {
+      posts: [],
+      error: '',
+      text: '',
+    };
+  },
+  async created() {
+    try {
+      this.posts = await PostService.getPosts();
+    } catch (err) {
+      this.error = err.message;
+    }
+  },
+  methods: {
+    async createPost() {
+      await PostService.insertPost(this.text);
+      this.posts = await PostService.getPosts();
+    },
+    async deletePost(id) {
+      await PostService.deletePost(id);
+      this.posts = await PostService.getPosts();
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
